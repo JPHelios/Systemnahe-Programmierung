be07c567f5163f31bb13a4162861ddac560a78fb
I538909 <43265991+JPHelios@users.noreply.github.com>
2022-05-09 14:35:45 +0200

    Version

--- TicTacToe.asm
+++ TicTacToe.asm
@@ -13,7 +13,49 @@ mov R4, #0DBH
 mov R5, #00H
 mov R6, #0DBH
 mov R7, #0DBH
-JMP display
+JMP anfang
+
+
+anfang:
+; Abfrage 1te Zeile
+	CLR P2.0
+; jetzt jede Spalte prüfen
+        JB P2.4,Check1
+        MOV A, #01h
+Check1: JB P2.5,Check2
+        MOV A, #02h
+Check2: JB P2.6, Check3 
+        MOV A, #03h
+Check3: JB P2.7, Check4
+        MOV A, #0Ah
+Check4: Setb P2.0
+
+; Abfrage 2te Zeile
+        CLR P2.1
+; jetzt jede Spalte prüfen
+        JB P2.4,Check5
+        MOV A, #04h
+Check5: JB P2.5,Check6
+        MOV A, #05h
+Check6: JB P2.6,Check7 
+        MOV A, #06h
+Check7: JB P2.7, Check8
+        MOV A, #0Bh
+Check8: Setb P2.1
+
+; Abfrage 3te Zeile
+        CLR P2.2
+; jetzt jede Spalte prüfen
+        JB P2.4,Check9
+        MOV A, #07h
+Check9: JB P2.5,Check10
+        MOV A, #08h
+Check10: JB P2.6, Check11 
+        MOV A, #09h
+Check11: JB P2.7, Check12
+        MOV A, #0Ch
+Check12: Setb P2.2
+
 
 display:
 mov P0, R0
@@ -48,7 +90,7 @@ mov P0, R7
 clr p1.0
 setb p1.0
 
-JMP display
+JMP anfang
 
 
 
